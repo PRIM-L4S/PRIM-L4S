3020951a8eae96e9591e6a5d9addee2f52ed1659
Thomas Sauvage <thomas-commits@sauvage.pm>
2026-04-15 18:03:00 +0200

    feat: 4 minutes per scenario

--- benchmarking-tools/serial_experiments_runner/src/constants.rs
+++ benchmarking-tools/serial_experiments_runner/src/constants.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
 pub const TIME_BETWEEN_SCENARIOS: Duration = Duration::from_secs(5);
-pub const RUN_TIME: Duration = Duration::from_secs(5);
+pub const RUN_TIME: Duration = Duration::from_mins(4);
 pub const MAX_UP_RETRIES: usize = 10;
 pub const UP_RETRY_WAIT: Duration = Duration::from_secs(10);
